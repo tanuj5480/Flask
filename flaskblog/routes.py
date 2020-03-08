fa6458a8cc49ac92f7cf4840dc3a6138c2382193
Tanuj <Tanuj5480@gmail.com>
2020-03-07 22:55:19 -0500

    Creating post level code

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,40 +1,20 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_login import login_user, current_user, logout_user, login_required
 from flaskblog.models import User, Post
 
 
-posts = [
-	{
-		'author': 'Tanuj Pandey',
-		'title': 'Learn to code',
-		'content': 'first post content',
-		'date_posted': 'February 24, 2020'
-	},
-	{
-		'author': 'TP1',
-		'title': 'Title 1',
-		'content': 'Bla bla',
-		'date_posted': 'February 24, 2020'
-	},
-	{
-		'author': 'TP1',
-		'title': 'Title 2',
-		'content': 'Bla bla',
-		'date_posted': 'February 24, 2020'
-	}
 
-
-]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts=posts)
+	posts = Post.query.all()
+	return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
@@ -110,3 +90,42 @@ def account():
 		form.email.data = current_user.email
 	image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
 	return render_template('account.html', title='Account', image_file = image_file, form=form)
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(title=form.title.data, content=form.content.data, author=current_user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post has been created!', 'success')
+		return redirect(url_for('home'))
+	return render_template('create_post.html', title='New Post', form=form, legend='Update Post')
+
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+	post = Post.query.get_or_404(post_id)
+	return render_template('post.html', title = post.title, post = post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+	post = Post.query.get_or_404(post_id)
+	if post.author != current_user:
+		abort(403)
+	form=PostForm()
+	if form.validate_on_submit():
+		post.title = form.title.data
+		post.content = form.content.data
+		db.session.commit()
+		flash('Your post has been updated!', 'success')
+		return redirect(url_for('post', post_id=post.id))
+	elif request.method == 'GET':
+		form.title.data = post.title
+		form.content.data = post.content
+	return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')	

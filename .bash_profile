0524f3d657a9068cf3775219ea7c02cdf7f647d1
Tanuj <Tanuj5480@gmail.com>
2020-03-14 11:46:50 -0400

    11 completed

--- .bash_profile
+++ .bash_profile
@@ -1,3 +1,7 @@
 #! /usr/bin/env bash
 export EMAIL_USER='tanuj5480@gmail.com'
 export EMAIL_PASSWORD='5idnmmi1'
+
+export SECRET_KEY='eee132c443ae54c34a11d31592d91566'
+export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+
--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,29 +5,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'eee132c443ae54c34a11d31592d91566'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 
-mail =Mail(app)
+mail =Mail()
 
 
-from flaskblog.users.routes import users
-from flaskblog.posts.routes import posts
-from flaskblog.main.routes import main
+def create_app(config_class=Config):
+	app = Flask(__name__)
+	app.config.from_object(Config)
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+	db.init_app(app)
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
+
+	from flaskblog.users.routes import users
+	from flaskblog.posts.routes import posts
+	from flaskblog.main.routes import main
+	app.register_blueprint(users)
+	app.register_blueprint(posts)
+	app.register_blueprint(main)
+
+	return app
